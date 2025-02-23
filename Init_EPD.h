6315db9069142f8068293c4fdd1ff1b54045f9d3
paxant <78857260+paxant@users.noreply.github.com>
2025-02-23 12:40:52 +0700

    UPLOAD

---
+++ Init_EPD.h
@@ -0,0 +1,15 @@
+/*
+ * Init_EPD.h
+ *
+ *  Created on: Feb 18, 2025
+ *      Author: pov
+ */
+
+#ifndef BSP_INIT_EPD_H_
+#define BSP_INIT_EPD_H_
+
+#include "main.h"
+
+
+
+#endif /* BSP_INIT_EPD_H_ */
